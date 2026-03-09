4b02e6f81193677ee1523e8898f149173ac122a4
euler <leonhard@euler.com>
2026-03-09 21:51:28 +0100

    app.py first implementation, created register.html, minor changes on index.html

---
+++ app.py
@@ -0,0 +1,110 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
+import os
+
+app = Flask(__name__)
+app.secret_key = 'temprorary-secret-key'
+
+DB_USERS_PATH = 'data.db'
+
+# -------------------------------
+# Database config
+# -------------------------------
+
+def db_connect():
+    conn = sqlite3.connect(DB_USERS_PATH)
+    conn.row_factory = sqlite3.Row
+    return conn 
+
+def database_init():
+    with db_connect() as db:
+        db.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   name TEXT NOT NULL,
+                   email TEXT NOT NULL UNIQUE,
+                   password TEXT NOT NULL
+            )
+        ''')
+        db.commit()
+
+# -------------------------------
+
+
+
+# -------------------------------
+# Routes
+# -------------------------------
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form.get("name", "").strip()
+        email = request.form.get("email", "").strip()
+        repeat_email = request.form.get("repeat_email", "").strip()
+        password = request.form.get("password", "").strip()
+        repeat_password = request.form.get("repeat_password", "").strip()
+
+        ############################ Start errors check ############################
+        
+        check_nullness = [len(name), len(email), len(repeat_email), len(password), len(repeat_email)]
+
+        if 0 in check_nullness:
+            flash("Riempi ogni campo!", "error")
+            return redirect(url_for('register'))
+        
+        if email != repeat_email:
+            flash("Emails don't match!", "error")
+            return redirect(url_for('register'))
+        
+        if password != repeat_password:
+            flash("Passwords don't match!", "error")
+            return redirect(url_for('register'))
+        
+        if len(password) < 6:
+            flash("At least 6 characters!", "error")
+            return redirect(url_for('register'))
+        
+        with db_connect() as db:
+            double_check = db.execute(
+                'SELECT id FROM users where email = ?', (email,)
+            ).fetchone() 
+        
+        if double_check:
+            flash("Email already exists, login!", "error")
+            return redirect(url_for('register'))
+        
+        ############################ End errors check ############################
+        
+        ############################ Start DB update ############################
+        
+        passwd_hash = generate_password_hash(password)
+        with db_connect() as db:
+            db.execute(
+                'INSERT INTO users (name, email, password) VALUES (?, ?, ?)', (name, email, passwd_hash)
+            )
+            db.commit()
+
+        flash("Registration succeded, login!", 'success')
+        return redirect(url_for('register'))
+
+        ############################ End DB update ############################
+    
+    return render_template("register.html")
+        
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+# -------------------------------
+
+
+if __name__ == '__main__':
+    database_init()
+    app.run(debug=True, port=1234)
---
+++ lazygit.sh
@@ -0,0 +1,4 @@
+git add -A
+read -p "Commit message: " COMMIT
+git commit -m $COMMIT
+git push
